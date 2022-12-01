e8744f72ab4b0738827b8ead216821f20b6fad71
Evellyn <evellyn.d.s.d@gmail.com>
2022-12-01 16:47:14 -0300

    terminando

--- src/br/senai/sp/jandira/dao/MedicoDAO.java
+++ src/br/senai/sp/jandira/dao/MedicoDAO.java
@@ -1,5 +1,189 @@
 package br.senai.sp.jandira.dao;
+
+import br.senai.sp.jandira.model.Medico;
+import br.senai.sp.jandira.model.PlanoDeSaude;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import javax.imageio.IIOException;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 public class MedicoDAO {
     
+    private final static String URL = "C:\\Users\\22282219\\Java\\Medico.txt";
+    private final static String URL_TEMP = "C:\\Users\\22282219\\java\\Medico-temp.txt";
+    private final static Path PATH = Paths.get(URL);
+    private final static Path PATH_TEMP = Paths.get(URL_TEMP);
+    /*
+    Essa classe será responsável pela persistência de dados
+    dos planos de saude, por exemplo, adicionar um novo,
+    excluir um plano de saude, etc.
+     */
     
-}
+    private static ArrayList<Medico> medicos = new ArrayList<>();
+    
+     public static void gravar(Medico m) { // CREATE
+        medicos.add(m);
+
+        try {
+            BufferedWriter escritor = Files.newBufferedWriter(
+                    PATH,
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.WRITE);
+
+            escritor.write(m.getMedicoSeparadoPorPontoEVirgula());
+            escritor.newLine();
+            escritor.close();
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro");
+        }
+    }
+    
+    
+    
+    public static ArrayList<Medico> getMedico() { // READ
+        return medicos;
+                
+    }
+
+    public static Medico getMedico(Integer codigo) { // READ
+
+        for (Medico m : medicos) {
+            if (m.getCodigo() == codigo) {
+                return m;
+            }
+
+        }
+
+        return null;
+
+    }
+    
+    public static void atualizar(Medico medicosAtualizado) { // UPDATE
+
+        for (Medico m : medicos) {
+            if (m.getCodigo() == medicosAtualizado.getCodigo()) {
+                medicos.set(medicos.indexOf(m), medicosAtualizado);
+                break;
+            }
+
+        }
+        
+//        atualizarArquivo();
+        
+    }
+
+    public static void excluir(Integer codigo) { // DELETE
+
+        for (Medico m : medicos) {
+            if (m.getCodigo().equals(codigo)) {
+                medicos.remove(m);
+                break;
+            }
+        }
+
+        atualizarArquivo();
+        
+    }
+    
+    private static void atualizarArquivo(){
+        
+        File arquivoAtual = new File(URL);
+        File arquivoTemp = new File(URL_TEMP);
+        
+        try {
+            
+            arquivoTemp.createNewFile();
+            
+            BufferedWriter bwTemp = Files.newBufferedWriter(
+                    PATH_TEMP, 
+                    StandardOpenOption.APPEND,
+                    StandardOpenOption.WRITE);
+            
+            for(Medico m : medicos){
+                bwTemp.write(m.getMedicoSeparadoPorPontoEVirgula());
+                bwTemp.newLine();
+            }
+            
+            arquivoAtual.delete();
+            
+            arquivoTemp.renameTo(arquivoAtual);
+            
+            bwTemp.close();
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    
+    // Criar uma lista inicial de medicos
+    public static void criarListaDeMedicos() {
+
+            
+        try {
+            BufferedReader leitor = Files.newBufferedReader(PATH);
+            
+            String linha = leitor.readLine();
+            
+            while(linha !=null){
+                
+                String[] vetor = linha.split(";");
+                String[] data = vetor[5].split("-");
+                
+                Medico m = new Medico(
+                        Integer.valueOf(vetor[0]),
+                        vetor[2],
+                        vetor[4],
+                        vetor[3],
+                        vetor[1],
+                        LocalDate.of(Integer.parseInt(data[0]),Integer.parseInt(data[1]),Integer.parseInt(data[2])));
+                
+                medicos.add(m);
+                
+                linha = leitor.readLine();
+                
+            }
+            
+            leitor.close();
+            
+        } catch( IOException e){
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Ocorreu um erro ao ler o arquivo");
+          } 
+
+
+    }
+    
+    public static DefaultTableModel getTabelaMedicos() {
+
+        System.out.println(medicos.size());
+
+        String[] titulo = {"CÓDIGO", "CRM", "NOME", "TELEFONE"};
+        String[][] dados = new String[medicos.size()][4];
+
+        for (Medico m : medicos) {
+            int i = medicos.indexOf(m);
+            dados[i][0] = m.getCodigo().toString();
+            dados[i][1] = m.getCrm();
+            dados[i][2] = m.getNome();
+            dados[i][3] = m.getTelefone();
+
+        }
+
+        return new DefaultTableModel(dados, titulo);
+    }
+ }
+    
+
--- src/br/senai/sp/jandira/model/Medico.java
+++ src/br/senai/sp/jandira/model/Medico.java
@@ -1,12 +1,19 @@
 package br.senai.sp.jandira.model;
 
+import java.time.LocalDate;
+
 public class Medico {
 
 	private String nome;
 	private String telefone;
 	private String email;
 	private String crm;
+        private Integer codigo = 100;
 	private Especialidade[] especialidade;
+
+    public Medico(Integer valueOf, String string, String string0, String string1, String string2, LocalDate of) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 	public String getNome() {
 		return nome;
@@ -31,6 +38,15 @@ public class Medico {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+        
+        
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
 
 	public String getCrm() {
 		return crm;
@@ -47,5 +63,9 @@ public class Medico {
 	public void setEspecialidade(Especialidade[] especialidade) {
 		this.especialidade = especialidade;
 	}
+
+    public char[] getMedicoSeparadoPorPontoEVirgula() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
